68d17cb97854fa1d8a087fb6a3ea291223f1836e
Fabscii <129980914+FabianSchieder@users.noreply.github.com>
2024-06-12 21:47:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -108,3 +108,22 @@ void checkPointer(void* pointer)
         exit(EXIT_FAILURE);
     }
 }
+
+
+//V2
+//int strcmp(char* str1, char* str2)
+//{
+//    if(strlen( str1) != strlen(str2))
+//    {
+//        return 0;
+//    }
+//    for(int i = 0; i < strlen(str1);i++)
+//    {
+//        if(str1[i] != str2[i])
+//        {
+//            return 0;
+//        }
+//
+//    }
+//    return 1;
+//}
